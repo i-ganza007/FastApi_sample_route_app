273e5fee9d0fbdb17390ab7caab7d492e297135f
Ian Ganza <144003894+i-ganza007@users.noreply.github.com>
2025-01-12 07:57:37 +0000

    Added the delete route , updated the update route & added the turtle update module

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI , HTTPException
 import uvicorn
+from turtle import update
 from pydantic import BaseModel
 from typing import List , Optional
 from uuid import UUID , uuid4
-#from uvicorn import 
 
 app = FastAPI()
 
@@ -28,13 +28,30 @@ def read_tasks():
     return {'All tasks inside':tasks}
 
 @app.put(f'/tasks/{id}',response_model=Task)
-def update_task(task_)
+def update_task(id:UUID,task_updated:Task):
+    for idx,task in enumerate(tasks):
+        if task.id == id:
+            new_task = task.copy(update(task_updated.dict(exclude_unset=True) ) )
+            tasks[idx] = new_task
+            return new_task
+    raise HTTPException(status_code=404, detail="Task not found")
 
 @app.get(f'/tasks/{id}',response_model=Task)
 def get_task(id:UUID):
     result = list(filter(lambda x:x.uuid == id , tasks))
     if len((result)) == 0 : raise HTTPException(status_code=404,detail='No task found')
     return result[0]
+
+@app.delete('/tasks/{id}', response_model=Task)
+def delete_task(id: UUID):
+    task_to_remove = next((task for task in tasks if task.id == id), None)
+    
+    if task_to_remove is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    
+    tasks.remove(task_to_remove)
+    
+    return task_to_remove
 
 if __name__== '__main__':
     uvicorn.run(app)
